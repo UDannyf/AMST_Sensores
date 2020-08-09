3610256102f8de1303c9623808ee1badd58c8a65
UDannyf <danny_u89@hotmail.com>
2020-08-09 02:19:36 -0500

    Final

--- app/src/main/java/com/example/giroscopio/MainActivity.java
+++ app/src/main/java/com/example/giroscopio/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.giroscopio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Pelota dibujo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        dibujo = new Pelota(this);
+        setContentView(dibujo);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
